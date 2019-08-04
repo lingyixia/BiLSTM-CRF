0b93e21beeab6ecba152a48749c9173760f7fac1
陈飞宇 <chinachenfeiyu@outlook.com>
2019-08-04 21:56:31 +0800

    Update Model.py

--- Model.py
+++ Model.py
@@ -2,11 +2,13 @@ import tensorflow as tf
 import tensorflow.contrib.crf as crf
 from tf_metrics import precision, recall, f1
 from tensorflow.contrib import keras
+from tensorflow.python.ops.rnn import bidirectional_dynamic_rnn
 
 
 class BiLSTMCrf(object):
     def __init__(self, inputX, inputY, sentenceLengths, numClasses, vocabSize, embeddingSize,
-                 hiddenSize, base_learn_rate, maxLength, l2_reg_lambda, dropout_keep_prob, crf, layer_size, eval_tags):
+                 hiddenSize, base_learn_rate, maxLength, l2_reg_lambda, dropout_keep_prob, crf, layer_size, eval_tags,
+                 random_seed):
         self.__num_classes = numClasses
         self.__vocab_size = vocabSize
         self.__embedding_size = embeddingSize
@@ -21,6 +23,7 @@ class BiLSTMCrf(object):
         self.__crf = crf
         self.__layer_size = layer_size
         self.__eval_tags = eval_tags
+        self.random_seed = random_seed
         self.__addEmbeddingLayer()
         self.__addBiLSTMLayer()
 
@@ -28,20 +31,23 @@ class BiLSTMCrf(object):
         with tf.name_scope('embeddingLayer'):
             embedding = tf.get_variable(name='embedding', dtype=tf.float32,
                                         shape=[self.__vocab_size, self.__embedding_size],
-                                        initializer=tf.contrib.layers.xavier_initializer())
+                                        initializer=tf.contrib.layers.xavier_initializer(seed=self.random_seed))
             embeddingInput = tf.nn.embedding_lookup(embedding, self.__inputX)
-            self.embeddingInput = tf.nn.dropout(embeddingInput, rate=1 - self.__dropout_keep_prob)
+            self.embeddingInput = tf.nn.dropout(embeddingInput, seed=self.random_seed,
+                                                rate=1 - self.__dropout_keep_prob)
 
     def __addBiLSTMLayer(self):
         with tf.name_scope('BiLSTMLayer'):
             bi_layer_size = self.__layer_size // 2
             lstm_fw_cell = tf.nn.rnn_cell.MultiRNNCell(
                 [tf.nn.rnn_cell.DropoutWrapper(cell=tf.nn.rnn_cell.BasicLSTMCell(num_units=self.__hidden_size),
-                                               output_keep_prob=self.__dropout_keep_prob) for _ in
+                                               output_keep_prob=self.__dropout_keep_prob, seed=self.random_seed) for _
+                 in
                  range(bi_layer_size)])
             lstm_bw_cell = tf.nn.rnn_cell.MultiRNNCell(
                 [tf.nn.rnn_cell.DropoutWrapper(cell=tf.nn.rnn_cell.BasicLSTMCell(num_units=self.__hidden_size),
-                                               output_keep_prob=self.__dropout_keep_prob) for _ in
+                                               output_keep_prob=self.__dropout_keep_prob, seed=self.random_seed) for _
+                 in
                  range(bi_layer_size)])
             bidOutput, bidCurrent_state = tf.nn.bidirectional_dynamic_rnn(cell_fw=lstm_fw_cell,
                                                                           cell_bw=lstm_bw_cell,
@@ -49,31 +55,47 @@ class BiLSTMCrf(object):
                                                                           inputs=self.embeddingInput,
                                                                           dtype=tf.float32)
             BiLSTMOutput = tf.concat(bidOutput, axis=-1)
-            self.BiLSTMOutput = tf.nn.dropout(BiLSTMOutput, rate=1 - self.__dropout_keep_prob)
+            self.BiLSTMOutput = tf.nn.dropout(BiLSTMOutput, seed=self.random_seed, rate=1 - self.__dropout_keep_prob)
 
     def __addBiLSTMOutPutDenseLayer(self, mode):
         with tf.name_scope('BiLSTMOutputDenseLayer'):
             self.bilstmDenseOutput = keras.layers.Dense(units=self.__num_classes,
-                                                        kernel_initializer=tf.contrib.layers.xavier_initializer())(
-                self.BiLSTMOutput)
+                                                        kernel_initializer=tf.contrib.layers.xavier_initializer(
+                                                            seed=self.random_seed))(self.BiLSTMOutput)
+            # self.bilstmDenseOutput = tf.layers.batch_normalization(bilstmDenseOutput,
+            #                                                        training=(mode == tf.estimator.ModeKeys.TRAIN),
+            #                                                        momentum=0.9)
             self.sequence = tf.argmax(self.bilstmDenseOutput, axis=-1)
             if mode in (tf.estimator.ModeKeys.TRAIN, tf.estimator.ModeKeys.EVAL):
                 losses = tf.nn.sparse_softmax_cross_entropy_with_logits(labels=self.__inputY,
                                                                         logits=self.bilstmDenseOutput)
                 mask = tf.sequence_mask(self.__sentence_lengths)
                 losses = tf.boolean_mask(losses, mask)
-                self.loss = tf.reduce_mean(losses)
+                var_lists = tf.trainable_variables()
+                l2_loss = tf.contrib.layers.apply_regularization(tf.contrib.layers.l2_regularizer(self.l2_reg_lambda),
+                                                                 var_lists)
+                self.loss = losses + l2_loss
 
     def __addCrfLayer(self, mode):
         with tf.name_scope('CRFLayer'):
-            logLikelihood, self.transitionParams = crf.crf_log_likelihood(self.bilstmDenseOutput,
-                                                                              self.__inputY,
-                                                                              self.__sentence_lengths)
+            with tf.variable_scope('transitions', reuse=tf.AUTO_REUSE):
+                self.transitionParams = tf.get_variable("transitions",
+                                                        shape=[self.__num_classes, self.__num_classes],
+                                                        initializer=tf.contrib.layers.xavier_initializer(
+                                                            seed=self.random_seed))
             self.sequence, _ = crf.crf_decode(self.bilstmDenseOutput,
                                               self.transitionParams,
                                               self.__sentence_lengths)
             if mode in (tf.estimator.ModeKeys.TRAIN, tf.estimator.ModeKeys.EVAL):
+                logLikelihood, self.transitionParams = crf.crf_log_likelihood(self.bilstmDenseOutput,
+                                                                              self.__inputY,
+                                                                              self.__sentence_lengths,
+                                                                              self.transitionParams)
                 self.loss = tf.reduce_mean(-logLikelihood)
+                var_lists = tf.trainable_variables()
+                l2_loss = tf.contrib.layers.apply_regularization(tf.contrib.layers.l2_regularizer(self.l2_reg_lambda),
+                                                                 var_lists)
+                self.loss += l2_loss
 
     def getResult(self, mode):
         self.__addBiLSTMOutPutDenseLayer(mode)
@@ -92,9 +114,9 @@ class BiLSTMCrf(object):
                 'f1': f1(labels=self.__inputY, predictions=self.sequence, num_classes=self.__num_classes,
                          pos_indices=self.__eval_tags, weights=weights)
             }
+            for metric_name, op in metrics.items():
+                tf.summary.scalar(metric_name, op[1], family=tf.estimator.ModeKeys.TRAIN)
             if mode == tf.estimator.ModeKeys.TRAIN:
-                for metric_name, op in metrics.items():
-                    tf.summary.scalar(metric_name, op[1])
                 learn_rate = tf.train.exponential_decay(self.__base_learn_rate,
                                                         tf.train.get_global_step(),
                                                         500,
